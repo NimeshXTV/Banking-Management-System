128900af582f117349f50ef1229157df70c39b62
Nimesh <nimeshsharma760@gmail.com>
2026-01-22 21:00:19 +0530

    Created a class to hold database credentials

--- src/BankingApp.java
+++ src/BankingApp.java
@@ -1,14 +1,8 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class BankingApp {
-
-    private static final String url = "";
-    private static final String username = "";
-    private static final String password = "";
-
     public static void main(String[] args) {
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -16,42 +10,37 @@ public class BankingApp {
         catch (ClassNotFoundException e){
             System.out.println(e.getMessage());
         }
-        try{
-            Connection connection = DriverManager.getConnection(url,username,password);
-            Scanner sc = new Scanner(System.in);
-            User user = new User(connection,sc);
-            Accounts accounts  = new Accounts(connection,sc);
-            AccountManager accountManager = new AccountManager();
+        Connection connection = DBconnnection.databaseConnection();
+        Scanner sc = new Scanner(System.in);
+        User user = new User(connection,sc);
+        Accounts accounts  = new Accounts(connection,sc);
+        AccountManager accountManager = new AccountManager();
 
-            while(true){
-                System.out.println("WELCOME TO BANKING MANAGEMENT SYSTEM");
-                System.out.println("1. Register");
-                System.out.println("2. Login");
-                System.out.println("3. Exit");
-                System.out.println();
-                System.out.println("Enter your choice : ");
-                int choice = sc.nextInt();
-                switch (choice){
-                    case 1:
-                        user.register();
-                        break;
+        while(true){
+            System.out.println("WELCOME TO BANKING MANAGEMENT SYSTEM");
+            System.out.println("1. Register");
+            System.out.println("2. Login");
+            System.out.println("3. Exit");
+            System.out.println();
+            System.out.println("Enter your choice : ");
+            int choice = sc.nextInt();
+            switch (choice){
+                case 1:
+                    user.register();
+                    break;
 
-                    case 2:
-                        String email = user.login();
-                        if(email!=null){
-                            System.out.println("User Logged In !");
-                        }
+                case 2:
+                    String email = user.login();
+                    if(email!=null){
+                        System.out.println("User Logged In !");
+                    }
 
-                    case 3:
-                        System.out.println("THANKS FOR USING BANKING SYSTEM :)");
-                        System.out.println("EXITING SYSTEM!");
-                        return;
-                }
-
+                case 3:
+                    System.out.println("THANKS FOR USING BANKING SYSTEM :)");
+                    System.out.println("EXITING SYSTEM!");
+                    return;
             }
-        }
-        catch (SQLException e) {
-            e.printStackTrace();
+
         }
     }
 }
